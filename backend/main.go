53b0318f7d525e9bc7d3380ddd29424cf15ab343
HinataYasukawa <h1i2n2a62208@gmail.com>
2025-03-10 11:19:27 +0900

    レビュー一覧が表示されないエラーを解決

--- backend/main.go
+++ backend/main.go
@@ -5,9 +5,27 @@ import (
 	"dokusyo/backend/models"
 	"dokusyo/backend/routes"
 	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
+
+// CORSミドルウェア
+func corsMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization")
+
+		// OPTIONSメソッドのリクエストには200を返して終了
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusOK)
+			return
+		}
+
+		c.Next()
+	}
+}
 
 func main() {
     // データベース接続
@@ -21,8 +39,12 @@ func main() {
 
     r := gin.Default()
 
+    // CORS ミドルウェアの追加
+    r.Use(corsMiddleware())
+
     // ルーティングの設定
     routes.SetupRoutes(r)
 
+    // サーバーの起動
     r.Run(":8080")
 }
